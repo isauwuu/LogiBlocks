7e4628b5e9895e96d156e8d81c6490087b386785
rchgst <quispericardo682@gmail.com>
2026-07-03 02:33:06 -0300

    add components - ui design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Canvas from './components/Canvas';
 import CodeViewer from './components/CodeViewer';
 import Console from './components/Console';
+import LeftPanel from './components/LeftPanel';
+import RightPanel from './components/RightPanel';
 import Toolbox from './components/Toolbox';
 
 export default function App() {
     return (
         <div>
-            <h1>DGS</h1>
             <Toolbox />
+            <LeftPanel />
             <Canvas />
+            <RightPanel />
             <Console />
             <CodeViewer code=""/>
         </div>
---
+++ src/components/LeftPanel.tsx
@@ -0,0 +1,3 @@
+export default function LeftPanel() {
+    return <div>LeftPanel</div>;
+}
